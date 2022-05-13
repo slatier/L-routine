4347fbeaece1e5341bfe9aa24918e1d64f2195b1
slatier <jackson.slater@icloud.com>
2022-05-13 09:06:00 -0400

    continued work on assignment

--- robot.py
+++ robot.py
@@ -13,7 +13,10 @@ class Robot(wpilib.TimedRobot):
         self.drivetrain = Drivetrain()
 
     def robotInit(self):
-        pass
+        self.drivetrain.m_left_encoder.setPosition(0)
+        self.drivetrain.m_right_encoder.setPosition(0)
+
+        self.turn_controller = PIDController(P, I, D)
 
     def robotPeriodic(self):
         pass
@@ -22,17 +25,25 @@ class Robot(wpilib.TimedRobot):
         pass
 
     def teleopPeriodic(self):
-        speed=-self.joy1.getRawAxis(1)*.4
-        turn=self.joy1.getRawAxis(4)*.2
+        speed=-self.joy1.getRawAxis(1) * .4
+        turn=self.joy1.getRawAxis(4) * .2
         #print(f"Speed: {speed} Turn: {turn}")
         self.drivetrain.set(speed+turn, speed-turn)
 
     def autonomousInit(self):
-        pass
+        wheel_diameter = 4  # inches
+        wheel_circumference = wheel_diameter * math.pi
+        leg_1_distance = (84)
+        leg_1_clicks = (leg_1_distance / wheel_circumference) * 360
+        self.drivetrain.set(leg_1_clicks, leg_1_clicks)
+        # self.drivetrain.gyro.setYaw(O)
+        # self.drivetrain.m_right_encoder(0)
+
 
     def autonomousPeriodic(self):
-        pass
-
+        self.turn_controller.setSetpoint(90)
+        turn_clicks = self.turn_controller.calculate(measurement=self.drivetrain.gyro.setYaw())
+        self.drivetrain.set(-turn_clicks, turn_clicks)
 
 if __name__ == "__main__":
     wpilib.run(Robot)
